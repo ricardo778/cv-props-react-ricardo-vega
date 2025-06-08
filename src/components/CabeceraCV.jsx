3e75a3e2f0ce113df2599fd5b9a20c74c4391836
av164702gmail.com <av164702gmail.com>
2025-06-07 20:46:23 -0500

    feat: componente CabeceraCV ahora recibe props

--- src/components/CabeceraCV.jsx
+++ src/components/CabeceraCV.jsx
@@ -1,8 +1,10 @@
-export default function CabeceraCV() {
+// src/components/CabeceraCV.jsx
+export default function CabeceraCV(props) {
   return (
     <header>
-      <h1>Ricardo Andres Vega Perez</h1>
-      <h2>Desarrollador de Software</h2>
+      <h1>{props.nombre}</h1>
+      <h2>{props.profesion}</h2>
+      <hr />
     </header>
   );
 }
